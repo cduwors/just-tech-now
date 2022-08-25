f841002137741edc6e2cb9bd660a8b62ddf3159b
cduwors <christinaduwors@gmail.com>
2022-08-25 14:08:04 -0400

    post feature complete and tested

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,6 +5,7 @@ const { User, Post } = require("../../models");
 router.get("/", (req, res) => {
 	Post.findAll({
 		attributes: ["id", "post_url", "title", "created_at"],
+		order: [["created_at", "DESC"]],
 		//include property is like JOIN in SQL
 		include: [
 			{
@@ -71,6 +72,25 @@ router.put("/:id", (req, res) => {
 			},
 		}
 	)
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: "No post found with this id" });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	Post.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
 		.then((dbPostData) => {
 			if (!dbPostData) {
 				res.status(404).json({ message: "No post found with this id" });
