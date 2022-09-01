1a7b5357d7597d2e2c3c5c27feb010c51cb9f97f
cduwors <christinaduwors@gmail.com>
2022-09-01 13:53:46 -0400

    single post routes and conditionals complete

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -14,16 +14,19 @@ router.get("/", (req, res) => {
 
 //POST/api/comments
 router.post("/", (req, res) => {
-	Comment.create({
-		comment_text: req.body.comment_text,
-		user_id: req.body.user_id,
-		post_id: req.body.post_id,
-	})
-		.then((dbCommentData) => res.json(dbCommentData))
-		.catch((err) => {
-			console.log(err);
-			res.status(400).json(err);
-		});
+	//check session
+	if (req.session) {
+		Comment.create({
+			comment_text: req.body.comment_text,
+			user_id: req.session.user_id,
+			post_id: req.body.post_id,
+		})
+			.then((dbCommentData) => res.json(dbCommentData))
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	}
 });
 
 //delete one
